b704d8e3f625a5210f5a907a8b160fa6f4deceea
theAlbanianGigia <simandre001@gmail.com>
2020-10-01 11:47:22 +0000

    first setup

---
+++ js/helpers.js
@@ -0,0 +1,15 @@
+
+const getParams = () => {
+  const url = window.location.search
+  let key = ""
+  return url.substring(1).split("=").reduce((acc, e, i) => {
+    i++
+    if (!(i % 2)) {
+      acc[key] = e
+    } else {
+      acc[e] = ""
+      key = e
+    }
+    return acc
+  }, {})
+}
